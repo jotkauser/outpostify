a66043753f909e90f56e6a9b11f412c51da81432
motylek <realmotylek@gmail.com>
2026-03-23 19:00:40 +0100

    Fix empty backstack error

--- app/src/main/java/ovh/motylek/outpostify/ui/screens/main/Navigation.kt
+++ app/src/main/java/ovh/motylek/outpostify/ui/screens/main/Navigation.kt
@@ -28,6 +28,7 @@ internal fun EntryProviderScope<Route>.screens(
             scope = screenScope,
             onSuccess = {
                 backStack.clear()
+                backStack.add(Route.Parcels)
             }
         )
     }
