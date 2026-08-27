39d31bee4b873896a53a65e5f1933c27ab9c9bfa
motylek <realmotylek@gmail.com>
2026-08-27 12:33:29 +0200

    API: Set target JVM to 21

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_21
     }
 }
 
